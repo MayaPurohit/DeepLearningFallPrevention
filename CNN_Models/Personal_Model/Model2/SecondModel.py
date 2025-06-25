e4ec727e49118252dbec0bdbaf3931558a2c9068
Maya Purohit <mayamotog1@gmail.com>
2025-06-25 11:39:31 -0400

    Second Model

--- CNN_Models/Personal_Model/Model2/SecondModel.py
+++ CNN_Models/Personal_Model/Model2/SecondModel.py
@@ -19,34 +19,34 @@ class SelfAttention(nn.Module):
         self.embed_size = embed_size
         
         
-        # Linear transformation for query, key, and value 
-        self.query = nn.Linear(embed_size, embed_size)
-        self.key = nn.Linear(embed_size, embed_size)
-        self.value = nn.Linear(embed_size, embed_size)
+     
+        self.query = nn.Linear(self.embed_size, self.embed_size)
+        self.key = nn.Linear(self.embed_size, self.embed_size)
+        self.value = nn.Linear(self.embed_size, self.embed_size)
 
-    def scaled_dot_product_attention(Q, K, V, mask=None):
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
         d_k = Q.size(-1)
         scores = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(torch.tensor(d_k, dtype=torch.float32))
 
-        # Apply mask if provided (useful for masked self-attention in transformers)
+     
         if mask is not None:
             scores = scores.masked_fill(mask == 0, float('-inf'))
 
-        # Softmax to normalize scores, producing attention weights
+        #transform into proabilities
         attention_weights = F.softmax(scores, dim=-1)
         
-        # Compute the final output as weighted values
+        #get weighted values 
         output = torch.matmul(attention_weights, V)
         return output, attention_weights
     
     def forward(self, x, mask=None):
-        # Generate Q, K, V matrices
+        #make the query, key, and value models
         Q = self.query(x)
         K = self.key(x)
         V = self.value(x)
         
-        # Calculate attention using our scaled dot-product function
-        out, _ = scaled_dot_product_attention(Q, K, V, mask)
+        
+        out, _ = self.scaled_dot_product_attention(Q, K, V, mask)
         return out
     
   
@@ -70,11 +70,16 @@ class SecondModelCNN(nn.Module):
             nn.BatchNorm1d(num_features*2)
         )
 
+      
+
         self.mid_conv2 = nn.Sequential(
             nn.Conv1d(num_features*2, num_features*3, 3, 1, padding=1),
             nn.BatchNorm1d(num_features*3)
         )
-    
+
+        self.attention = SelfAttention(num_features*3)
+
+        self.pool = nn.AdaptiveAvgPool1d(1)
         self.fc_layer = nn.Linear(num_features*3, num_features*3)
         self.fc_layer2 = nn.Linear(num_features*3, num_classes)
        
@@ -101,10 +106,19 @@ class SecondModelCNN(nn.Module):
         x = self.mid_conv(x)
         
 
+
         x = self.mid_conv2(x)
 
+        x = x.permute(2, 0, 1)  
 
-        x = torch.mean(x, dim=2)
+        x  = self.attention(x)
+        
+        x = x.permute(1, 2, 0)
+
+
+        x = self.pool(x)
+        x = x.squeeze(-1)   
+        x = torch.flatten(x, 1)
        
         x = self.fc_layer(x)
         x = self.fc_layer2(x)
