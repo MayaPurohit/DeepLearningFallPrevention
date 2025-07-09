1431a6209caf971957f4d49361c374c979205a17
Maya Purohit <mayamotog1@gmail.com>
2025-07-09 16:48:55 -0400

    Updated Dataloader with new data

--- CNN_Dataloader.py
+++ CNN_Dataloader.py
@@ -42,16 +42,20 @@ class MotionDataset(Dataset):
         random.seed(seed)
         np.random.seed(seed)
 
-        if test_type == "normal" or test_type == "individual":
+        if test_type == "normal" or test_type == "individual" or test_type == "olivia":
 
             if test_type == "normal":
                 self.full_dataset = self.create_dataset_normal()
-            else:
+            elif test_type == "individual":
                 self.full_dataset = self.create_dataset_individual()
+            elif test_type == "olivia":
+                self.full_dataset = self.create_dataset_olivia()
 
         
+            print(type(self.full_dataset))
             indices = np.arange(len(self.full_dataset))
             np.random.shuffle(indices)
+
 
             total_len = len(indices)
 
@@ -61,8 +65,11 @@ class MotionDataset(Dataset):
 
 
             train_ind = indices[:train_size]
+            print(train_ind)
+            print(train_ind.size)
             val_ind = indices[train_size:train_size + val_size]
             test_ind = indices[train_size + val_size:]
+
 
             self.test_data = self.full_dataset[test_ind]
             self.train_data = self.full_dataset[train_ind]
@@ -91,6 +98,9 @@ class MotionDataset(Dataset):
             self.val_data = self.full_dataset[val_ind]
         elif test_type == 'both':
             self.train_data, self.test_data, self.val_data = self.create_dataset_both()
+        elif test_type == "olivia":
+            self.train_data, self.test_data = self.create_dataset_olivia()
+
 
 
 
@@ -142,6 +152,7 @@ class MotionDataset(Dataset):
         
             df['Composed_Acceleration'] = np.sqrt(df['Shimmer_8665_Accel_LN_X_CAL']**2 + df['Shimmer_8665_Accel_LN_Y_CAL']**2 + df['Shimmer_8665_Accel_LN_Z_CAL']**2)
             df['Composed_Gyroscope'] = np.sqrt(np.power(df['Shimmer_8665_Gyro_X_CAL'], 2) + np.power(df['Shimmer_8665_Gyro_Y_CAL'], 2) + np.power(df['Shimmer_8665_Gyro_Z_CAL'], 2))
+            
             normalized_acceleration = (df['Composed_Acceleration'] - df['Composed_Acceleration'].mean())/ (df['Composed_Acceleration'].std())
             peaks, _ = find_peaks(normalized_acceleration, distance = self.window_size*2, prominence = 2)
 
@@ -177,12 +188,15 @@ class MotionDataset(Dataset):
                 normalized_acceleration = (df['Composed_Acceleration'] - df['Composed_Acceleration'].mean())/ (df['Composed_Acceleration'].std())
                 peaks, _ = find_peaks(normalized_acceleration, distance = self.window_size*2, prominence = 2)
 
+                k = 0
                 for idx in peaks:
                     if (idx - ((self.window_size//2)-1)) > 0:
                         if self.input_channels == 2:
-                            data_sample = df.iloc[idx - ((self.window_size//2)): idx + (self.window_size//2), 11:]
+                            data_sample = df.iloc[idx - ((self.window_size//2-1)): idx + (self.window_size//2+1), 11:]
+                            np.savetxt(f"C:\\Users\\mayam\\DeepLearningFallDetection\\SavedData\\{PersonList[i]}_{ActivityList[j]}_Normal_{k+1}.dat", data_sample, delimiter=',')
+                            k += 1
                         else:
-                            data_sample = df.iloc[idx - ((self.window_size//2)): idx + (self.window_size//2), 1:7]
+                            data_sample = df.iloc[idx - ((self.window_size//2-1)): idx + (self.window_size//2 +1), 1:7]
                         if self.normalize:
                             data_sample[:] = self.scaler.fit_transform(data_sample)
                         data_pack["data_sample"] = torch.tensor(data_sample.to_numpy(), dtype=torch.float32)
@@ -272,19 +286,20 @@ class MotionDataset(Dataset):
         dataset = []
         for j in range(len(ActivityList)):
             data_pack = {}
-            df = df = pd.read_csv(self.root_dir + fr"\\{chosen_ind}_{ActivityList[j]}_Normal.csv", sep = "\t", header = 1)
+            df = pd.read_csv(self.root_dir + fr"\\{chosen_ind}_{ActivityList[j]}_Normal.csv", sep = "\t", header = 1)
             df = df[1:].astype('float64')
         
             df['Composed_Acceleration'] = np.sqrt(df['Shimmer_8665_Accel_LN_X_CAL']**2 + df['Shimmer_8665_Accel_LN_Y_CAL']**2 + df['Shimmer_8665_Accel_LN_Z_CAL']**2)
             df['Composed_Gyroscope'] = np.sqrt(np.power(df['Shimmer_8665_Gyro_X_CAL'], 2) + np.power(df['Shimmer_8665_Gyro_Y_CAL'], 2) + np.power(df['Shimmer_8665_Gyro_Z_CAL'], 2))
-            
             normalized_acceleration = (df['Composed_Acceleration'] - df['Composed_Acceleration'].mean())/ (df['Composed_Acceleration'].std())
             peaks, _ = find_peaks(normalized_acceleration, distance = self.window_size*2, prominence = 2)
-
+            i = 0
             for idx in peaks:
                 if (idx - ((self.window_size//2)-1)) > 0:
                     if self.input_channels == 2:
                         data_sample = df.iloc[idx - ((self.window_size//2)): idx + (self.window_size//2), 11:]
+                        #np.savetxt(f"C:\\Users\\mayam\\DeepLearningFallDetection\\SavedData\\User1_{ActivityList[j]}_Normal_{i+1}.dat", data_sample, delimiter=',')
+                        i +=1
                     else:
                         data_sample = df.iloc[idx - ((self.window_size//2)): idx + (self.window_size//2), 1:7]
                     if self.normalize:
@@ -296,6 +311,35 @@ class MotionDataset(Dataset):
         dataset = np.array(dataset)
 
         return dataset
+
+
+    def create_dataset_olivia(self):
+
+        ActivityList = ['LocationA', 'LocationB', 'LocationC', 'LocationD', 'LocationE']
+        PersonList = ['User1', 'User2', 'User3', 'User4', 'User5']
+        NumSamplesPerLocation = [156, 147, 154, 160, 142, 152, 125, 155, 134, 121, 141, 100, 142, 151, 137, 145, 146, 160, 148, 144, 146, 141, 142, 141, 139]
+        dataset = []
+        l = -1
+        for i in range(len(PersonList)):
+            for j in range(len(ActivityList)):
+                l +=1 
+                for k in range(1, NumSamplesPerLocation[l] + 1):
+                    if self.root_dir + fr"\\{PersonList[i]}_{ActivityList[j]}_Normal_{k}.dat" == self.root_dir + fr"\\User4_LocationB_Normal_1.dat":
+                        continue
+                    data_pack = {}
+                    data_sample = np.loadtxt(self.root_dir + fr"\\{PersonList[i]}_{ActivityList[j]}_Normal_{k}.dat", delimiter = ',')
+                    data_pack["data_sample"] = torch.tensor(data_sample, dtype=torch.float32)
+                    data_pack["class_label"] = torch.tensor(j, dtype=torch.long)
+                    dataset.append(data_pack)
+
+
+        dataset = np.array(dataset)
+
+        
+
+        return dataset
+
+
 
     def __len__(self):
         if self.mode == "train":
