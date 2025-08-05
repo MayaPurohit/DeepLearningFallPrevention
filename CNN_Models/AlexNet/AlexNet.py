f3a515179bd7989c23c8b8b83490a1b3bd4eff2f
Maya Purohit <mayamotog1@gmail.com>
2025-08-04 20:04:41 -0400

    Our AlexNet Implementation

--- CNN_Models/AlexNet/AlexNet.py
+++ CNN_Models/AlexNet/AlexNet.py
@@ -12,49 +12,52 @@ import torch.nn.functional as F
 
 
 
+
 class AlexNetCNN(nn.Module):
-    def __init__(self, input_channels=6, num_classes=5):
+    def __init__(self, input_channels=6, num_classes=5, num_stacks = 2):
         """
   
         """
+
+        self.num_stack = num_stacks
         super(AlexNetCNN, self).__init__()
         
         #Kernel Size = 11, stride = 4
         self.first_conv = nn.Sequential(
-            nn.Conv1d(input_channels, 64, kernel_size=11, stride=4, padding=2),
-            nn.BatchNorm1d(64),
+            nn.Conv2d(1, 96, kernel_size=3, stride=4, padding=2),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride = 2),
+            nn.MaxPool2d(kernel_size=2, stride = 2),
             
         )
         
 
         self.second_conv = nn.Sequential(
-            nn.Conv1d(64, 192, kernel_size = 5, stride = 1, padding=2),
-            nn.BatchNorm1d(192),
+            nn.Conv2d(96, 256, kernel_size = 5, stride = 1, padding=2),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride = 2),
+            nn.MaxPool2d(kernel_size=1, stride = 2),
             
         )
 
         self.third_conv = nn.Sequential(
-            nn.Conv1d(192, 384, kernel_size = 3,padding=1),
+            nn.Conv2d(256, 384, kernel_size = 3,padding=1),
             nn.ReLU(),
         )
 
         self.fourth_conv = nn.Sequential(
-            nn.Conv1d(384, 256, kernel_size = 3, padding=1),
+            nn.Conv2d(384, 384, kernel_size = 3, padding=1),
             nn.ReLU(),
         )
 
         self.fifth_conv = nn.Sequential(
-            nn.Conv1d(256, 256, kernel_size = 3,  padding=1),
+            nn.Conv2d(384, 256, kernel_size = 3,  padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride = 2),
+            nn.MaxPool2d(kernel_size=1, stride = 2),
         )
 
         self.fc6 = nn.Sequential(
-            nn.Linear(256*3, 4096),
+            nn.Linear(256*2, 4096),
             nn.ReLU(),
             nn.Dropout()
         )
@@ -75,25 +78,31 @@ class AlexNetCNN(nn.Module):
     def _initialize_weights(self):
 
         for m in self.modules(): #initialize the appropriate weights
-            if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in', nonlinearity='relu')
                 if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm1d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-        pass
+                    nn.init.constant_(m.bias, 0)
+
+
+        
         
     def forward(self, x):
         
+       
+        x = x.view(x.size(0), 1, self.num_stack*2, 50)
+
         x = self.first_conv(x)
        
+        
         x = self.second_conv(x)
        
+      
         x = self.third_conv(x)
-         
+
+        
         x = self.fourth_conv(x)
 
+       
         x = self.fifth_conv(x)
 
 
