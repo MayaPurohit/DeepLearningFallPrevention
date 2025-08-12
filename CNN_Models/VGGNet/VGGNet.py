3f45b6338100c0be9e954fbded37b7f2b2f4307c
Maya Purohit <mayamotog1@gmail.com>
2025-08-11 20:07:49 -0400

    Add Model Architecture Documentation

--- CNN_Models/VGGNet/VGGNet.py
+++ CNN_Models/VGGNet/VGGNet.py
@@ -1,8 +1,8 @@
 
 
 # Maya Purohit
-# AlexNet
-# Develop classes to develop the AlexNet Architecture Model
+# VGGNet.py
+# Develop classes to replicate the VGGNet Architecture Model
 
 import torch
 import torch.nn as nn
@@ -13,93 +13,95 @@ import torch.nn.functional as F
 
 
 class VGGNetCNN(nn.Module):
-    def __init__(self, input_channels=6, num_classes=5):
+    def __init__(self, input_channels=6, num_classes=5, num_stacks = 2):
         """
-  
+        Model Layer initialization for VGGNet implementation
         """
+
+        self.num_stacks = num_stacks
         super(VGGNetCNN, self).__init__()
         
 
         self.first_conv = nn.Sequential(
-            nn.Conv1d(input_channels, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU()   
         )
         
 
         self.second_conv = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size = 3, stride = 1, padding=1),
+            nn.Conv2d(64, 64, kernel_size = 3, stride = 1, padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride = 2)
+            nn.MaxPool2d(kernel_size=2, stride = 2)
             
         )
 
       
         self.third_conv = nn.Sequential(
-            nn.Conv1d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU()   
         )
         
 
         self.fourth_conv = nn.Sequential(
-            nn.Conv1d(128, 128, kernel_size = 3, stride = 1, padding=1),
+            nn.Conv2d(128, 128, kernel_size = 3, stride = 1, padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride = 2)
+            nn.MaxPool2d(kernel_size=2, stride = 2)
         )
 
         self.fifth_conv = nn.Sequential(
-            nn.Conv1d(128, 256, kernel_size = 3,  padding=1),
+            nn.Conv2d(128, 256, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.sixth_conv = nn.Sequential(
-            nn.Conv1d(256, 256, kernel_size = 3,  padding=1),
+            nn.Conv2d(256, 256, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.seventh_conv = nn.Sequential(
-            nn.Conv1d(256, 256, kernel_size = 3,  padding=1),
+            nn.Conv2d(256, 256, kernel_size = 3,  padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride = 2)
+            nn.MaxPool2d(kernel_size=1, stride = 2)
             
         )
 
         self.eighth_conv = nn.Sequential(
-            nn.Conv1d(256, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(256, 512, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.ninth_conv = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(512, 512, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.tenth_conv = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(512, 512, kernel_size = 3,  padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride = 2)
+            nn.MaxPool2d(kernel_size=1, stride = 2)
             
         )
 
         
         self.eleventh_conv = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(512, 512, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.twelveth_conv = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(512, 512, kernel_size = 3,  padding=1),
             nn.ReLU()
             
         )
         self.thirteenth_conv = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size = 3,  padding=1),
+            nn.Conv2d(512, 512, kernel_size = 3,  padding=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride = 2)
+            nn.MaxPool2d(kernel_size=1, stride = 2)
             
         )
 
         self.fc1 = nn.Sequential(
-            nn.Linear(256*2, 4096),
+            nn.Linear(256*4, 4096),
             nn.ReLU(),
             nn.Dropout()
         )
@@ -118,18 +120,24 @@ class VGGNetCNN(nn.Module):
         pass
         
     def _initialize_weights(self):
-
+        '''
+        Initialize weights and biases for each type of layer
+        '''
         for m in self.modules(): #initialize the appropriate weights
-            if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm1d):
+            elif isinstance(m, nn.BatchNorm2d):
                 nn.init.ones_(m.weight)
                 nn.init.zeros_(m.bias)
         pass
         
     def forward(self, x):
+        '''
+        Forward pass through the model architecture
+        '''
+        x = x.view(x.size(0), 1, self.num_stacks*2, 50)
         
         x = self.first_conv(x)
        
